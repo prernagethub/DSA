46780eb979e495b188ade2e9434ddf641709a3ba
prerna rajput <rajputprerna1503@gmail.com>
2025-12-02 11:00:55 +0530

    reverse

--- app.js
+++ app.js
@@ -413,3 +413,15 @@ if (pr === null) {
 // }
 
 // console.log(sum);
+
+// let n = 123;
+// let rev = 0;
+// while (n > 0) {
+//   let rem = n % 10;
+//   rev = rev * 10 + rem;
+//   n = Math.floor(n / 10);
+// }
+
+// console.log(rev);
+
+
